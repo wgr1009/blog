ff6cef2e01a9309c0b59264c49220083c6cd6802
wgr_1009@163.com <wgr_1009@163.com>
2020-05-09 15:01:04 +0800

    设定每个用户只显示该用户下的文章和过滤分类

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -3,11 +3,23 @@ from django.urls import reverse
 from django.utils.html import format_html
 
 from blog.models import Post, Category, Tag
+from .adminforms import PostAdminForm
+from django.contrib import admin
+from typeidea.custom_site import custom_site
 
 
 # Register your models here.
-@admin.register(Category)
+
+class PostInline(admin.TabularInline):  # StacuedInline样式不同
+    fields = ('title', 'desc')
+    extra = 1  # 控制额外多几个
+    model = Post
+
+
+# @admin.register(Category)
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
+    inlines = [PostInline]
     list_display = ['name', 'owner', 'status', 'is_nav', 'created_time', 'post_count']
     fields = ['name', 'status', 'is_nav']  # 控制admin后台显示的字段
 
@@ -31,7 +43,8 @@ class CategoryAdmin(admin.ModelAdmin):
     post_count.short_description = '文章数量'
 
 
-@admin.register(Tag)
+# @admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     list_display = ['name', 'owner', 'status', 'created_time']
     fields = ['name', 'status']
@@ -42,17 +55,45 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Post)
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """
+     自定义过滤器只展示当前用户分类
+
+     SimpleListFilter类提供了两个属性和两个方法来供我们重写,
+     两个属性:
+        title：‘用于展示标题’
+        parameter_name: 是查询URL参数的名字, 比如查询分类id为1的内容时,URL后面的query部分是 ?owner_category=2, 此时就可以通过过滤器
+                        拿到这个id，从而进行过滤
+    两个方法:
+        lookups: 返回要展示的内容和查询用的id ( 就是上面query用的)
+        queryset: 根据URL Query的内容返回列表页数据,
+    """
+    title = '分类过滤器'
+    parameter_name = "owner_category"
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+# @admin.register(Post)
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
     # 配置列表页展示字段
     list_display = [
-        'title', 'category', 'status', 'created_time', 'operator'
+        'title', 'category', 'status', 'created_time', 'operator', 'owner'
     ]  # operator 自定义一个操作字段
 
     # 配置那些字段可以作为链接，点击可以进入编辑页面
     list_display_links = []
 
-    list_filter = ['category']  # 显示过滤器，根据那些字段进行过滤
+    # list_filter = ['category']  # 显示过滤器，根据那些字段进行过滤
+    list_filter = [CategoryOwnerFilter]  # 显示过滤器，根据那些字段进行过滤
     search_fields = ['title', 'category__name']  # 显示搜索栏，可以根据那些字段进行搜索
 
     # actions_on_top = True  # 动作相关的配置，是否现在在顶部
@@ -61,14 +102,56 @@ class PostAdmin(admin.ModelAdmin):
     # 编辑页面
     # save_on_top = True  # 保存，编辑，编辑及新建按钮是否在顶部展示，默认在底部展示
 
-    fields = [
-        ('category', 'title'), 'desc', 'status', 'content', 'tag'
-    ]
+    # exclude = ('owner', )
+
+    # fields两个作用, 一个是限定要展示的字段, 另外一个是配置展示字段的顺序, fields与fieldset任选其一
+    # fields = [
+    #     'category', 'title', 'desc', 'status', 'content', 'tag'
+    # ]
+
+    # fieldsets用来控制全局
+    # fieldsets格式： 第一个元素是string, 第二个元素是dict, dict的key可以是'fileds'/ description / classes
+    #                fields控制展示那些元素, classes是给要配置的版块加上一些css属性, django admin默认支持collapse、wide
+    # fieldsets = (
+    #     ('名称', {'内容'}),
+    #     ('名称', {'内容'})
+    # )
+    fieldsets = (
+        (
+            "基础配置", {
+                "description": "基础配置描述",
+                "fields": (
+                    ('title', 'category'),  # 元组带面两个字段显示为一行
+                    'status',
+                )
+            }
+        ),
+        (
+            '内容', {
+                'fields': (
+                    'desc',
+                    'content',
+                )
+            }
+        ),
+        (
+            '额外信息', {
+                'classes': ('collapse',),
+                'fields': ('tag',),
+            }
+        )
+    )
+
+    # filter_horizontal = ('tag', )
+    # filter_vertical = ('tag', )
+
+    # form = PostAdminForm  # 摘要多行多列展示
 
     def operator(self, obj):
         return format_html(
             "<a href='{}'>编辑</a>",
-            reverse('admin:blog_post_change', args=(obj.id,))
+            # reverse('admin:blog_post_change', args=(obj.id,))
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
 
     operator.short_description = '操作'  # 自定义字段的描述信息
@@ -77,4 +160,12 @@ class PostAdmin(admin.ModelAdmin):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
 
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
 
+    class Meta:
+        css = {
+            'all': ('https://bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css',),
+        }
+        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js',)
--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -15,11 +15,16 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='名称')
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')  # 只包含正整数
     is_nav = models.BooleanField(default=False, verbose_name='是否为导航')
+
+    # 作者 和 分类 为 一对多 的关系
     owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.DO_NOTHING)  # 外键关联，关联到django内部的User表
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     class Meta:
         verbose_name = verbose_name_plural = "分类"
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
@@ -32,11 +37,16 @@ class Tag(models.Model):
 
     name = models.CharField(max_length=10, verbose_name='名称')
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
+
+    # 作者 和 标签 为 一对多 的关系
     owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -53,11 +63,21 @@ class Post(models.Model):
     desc = models.CharField(max_length=1024, blank=True, verbose_name='摘要')
     content = models.TextField(verbose_name='正文', help_text='正文必须为MarkDown格式')
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
-    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.DO_NOTHING)  # 外键关联分类表
+
+    # 分类 和 文章 为 一对多 的关系
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.DO_NOTHING)
+
+    # 标签 和 文章 为多对多 的关系
     tag = models.ManyToManyField(Tag, verbose_name='标签')  # 外键关联标签表
+
+    # 作者 和 文章 为 一对多 的关系
     owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.DO_NOTHING)  # 关联用户表
+
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     class Meta:
         verbose_name = verbose_name_plural = '文章'
         ordering = ['-id']  # 根据id进行排序 -代表表示降序排列
+
+    def __str__(self):
+        return self.title
